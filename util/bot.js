bb1bf0c2352a9b6c3712bf9cc0fd4b4262e8c73e
Alfanaannet <134984185+Alfanaannet@users.noreply.github.com>
2024-02-04 04:29:42 +0200

    Update bot.js

--- util/bot.js
+++ util/bot.js
@@ -31,7 +31,7 @@ export default async (debug = false, config, settings, is_replit = (process.env.
   const isInteger = num => /^-?[0-9]+$/.test(num + '');
   const radio_channels = new Database("/radio");
   const client = new Client({
-    auth: "Bot " + "MTIwMzA3Njc1NzAzNzUxODg5OA.GK0_qz.RS0RITIXYgHkXo4QtGKqyn4biPQhmxUpdwd1Qk",
+    auth: "Bot " + process.env.token,
     rest: { requestTimeout: 60000 },
     gateway: { intents: ["GUILDS", "GUILD_VOICE_STATES"] }
   });
@@ -44,7 +44,7 @@ export default async (debug = false, config, settings, is_replit = (process.env.
   client.on("ready", async () => {
     if (spinner) spinner.success({ text: `Logged in as \x1B[1m${client.user.username}\u001b[0m (\x1B[4m${client.user.id}\u001b[0m)` });
     if (!ready_first) {
-      console.log(`\n\u001b[32;1m` + shuruhatik + `\u001b[0m\u001b[0m\n\n\u001b[1mﻲﺒﻨﻟﺍ ﻰﻠﻋ ةﻼﺻﻭ رﺎﻔﻐﺘﺳﻻﺍ ﺮﺜﻛﻭ ،ﻪﻠﻟﺍ ﺮﻛﺫ َﺲﻨﺗ ﻻ\u001b[0m`);
+      //console.log(`\n\u001b[32;1m` + shuruhatik + `\u001b[0m\u001b[0m\n\n\u001b[1mﻲﺒﻨﻟﺍ ﻰﻠﻋ ةﻼﺻﻭ رﺎﻔﻐﺘﺳﻻﺍ ﺮﺜﻛﻭ ،ﻪﻠﻟﺍ ﺮﻛﺫ َﺲﻨﺗ ﻻ\u001b[0m`);
       console.log(`\n\u001b[32;1m◤\u001b[0m\u001b[0m\u001b[1m https://api.shuruhatik.com/add/${client.user.id} \u001b[32;1m◢\u001b[0m`);
       ready_first = true;
       setInterval(async () => {
@@ -102,7 +102,7 @@ export default async (debug = false, config, settings, is_replit = (process.env.
       case 2: {
         if (interaction.data.name === "radio" && interaction.data.options.getSubCommand() == "stop") {
           if (await settings.has("guild_id") && await settings.get("guild_id") != interaction.guild.id) return await interaction.createMessage({
-            embeds: [{ color: 0xff3434, title: `:x: فقط سيرفر ذات معرف هذا ${await settings.get("guild_id")} يمكنه استخدام البوت هو فقط` }], flags: 64
+            embeds: [{ color: 0xff3434, title: `\`:x:\` فقط سيرفر ذات معرف هذا ${await settings.get("guild_id")} يمكنه استخدام البوت هو فقط` }], flags: 64
           })
           stop_radio = true;
           await settings.delete("radio");
@@ -113,7 +113,7 @@ export default async (debug = false, config, settings, is_replit = (process.env.
         }
         if (interaction.data.name === "radio" && interaction.data.options.getSubCommand() == "start") {
           if (await settings.has("guild_id") && await settings.get("guild_id") != interaction.guild.id) return await interaction.createMessage({
-            embeds: [{ color: 0xff3434, title: `:x: فقط سيرفر ذات معرف هذا ${await settings.get("guild_id")} يمكنه استخدام البوت هو فقط` }], flags: 64
+            embeds: [{ color: 0xff3434, title: `\`:x:\` فقط سيرفر ذات معرف هذا ${await settings.get("guild_id")} يمكنه استخدام البوت هو فقط` }], flags: 64
           })
           let channel = interaction.data.options.getString("channel");
           let voice = interaction.data.options.getChannel("voice");
@@ -260,10 +260,10 @@ export default async (debug = false, config, settings, is_replit = (process.env.
         voiceConnection.subscribe(player);
 
         player.on(AudioPlayerStatus.Playing, () => {
-          console.log("\n\u001b[32;1m▣\u001b[0m\u001b[0m\u001b[1m Started playing");
+         // console.log("\n\u001b[32;1m▣\u001b[0m\u001b[0m\u001b[1m Started playing");
         });
         player.on(AudioPlayerStatus.Idle, () => {
-          console.log("\n\u001b[32;1m▣\u001b[0m\u001b[0m\u001b[1m The player is not playing any audio");
+         // console.log("\n\u001b[32;1m▣\u001b[0m\u001b[0m\u001b[1m The player is not playing any audio");
         });
         if (stop_radio) {
           await player.stop();
@@ -295,18 +295,18 @@ export default async (debug = false, config, settings, is_replit = (process.env.
 
 
   process.on('unhandledRejection', (reason, p) => {
-    console.log('\u001b[38;5;93m[antiCrash] :: Unhandled Rejection/Catch\u001b[0m');
-    console.log(reason, p);
+    //console.log('\u001b[38;5;93m[antiCrash] :: Unhandled Rejection/Catch\u001b[0m');
+   // console.log(reason, p);
   });
 
   process.on("uncaughtException", (err, origin) => {
-    console.log('\u001b[38;5;93m[antiCrash] :: Uncaught Exception/Catch\u001b[0m');
-    console.log(err, origin);
+   // console.log('\u001b[38;5;93m[antiCrash] :: Uncaught Exception/Catch\u001b[0m');
+  //  console.log(err, origin);
   })
 
   process.on('uncaughtExceptionMonitor', (err, origin) => {
-    console.log('\u001b[38;5;93m[antiCrash] :: Uncaught Exception/Catch (MONITOR)\u001b[0m');
-    console.log(err, origin);
+   // console.log('\u001b[38;5;93m[antiCrash] :: Uncaught Exception/Catch (MONITOR)\u001b[0m');
+   // console.log(err, origin);
   });
 
 }
